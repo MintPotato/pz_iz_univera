4db5d19ca490ae2b6dbcbab2a58827d04f8912d8
ginger dog <104458002+MintPotato@users.noreply.github.com>
2023-01-12 06:36:33 +0300

    Update boba.py

--- laba1/boba.py
+++ laba1/boba.py
@@ -2,22 +2,22 @@ def recurs(spisok_chisel: list, chislo: int, otv: str, s: int) -> bool:
     global g_otv
 
     """Если число найдено, то Записываем порядок знаков в g_otv"""
-    if chislo == s and not spisok_chisel:
-        g_otv = otv
-        return True
-
-    elif spisok_chisel:
-
-        """Если число было найдено, возвращаем истину"""
-        if recurs(spisok_chisel[1:], chislo + spisok_chisel[0], otv + '+', s) or\
-                recurs(spisok_chisel[1:], chislo - spisok_chisel[0], otv + '-', s):
+    if not spisok_chisel:
+        if chislo == s:
+            g_otv = otv
             return True
+        else:
+            return False
+
+    """Если число было найдено, возвращаем истину"""
+    if recurs(spisok_chisel[1:], chislo + spisok_chisel[0], otv + '+', s) or \
+            recurs(spisok_chisel[1:], chislo - spisok_chisel[0], otv + '-', s):
+        return True
 
     return False  # иначе ложь
 
 
 def krasiviy_vyvod(chisla: list, znaki: str, s: int, n: int) -> str:
-
     otv = str(chisla[0])
 
     for i in range(1, n):
@@ -32,9 +32,8 @@ def main():
     запись его значений в отдельный список
     Закрытие файла"""
 
-    f = open("""Название файла""")
+    f = open('Имя файла', 'r+')
     a = list(map(int, list(f)[0].split()))
-    f.close()
 
     """Перенос значений из полученного выше списка, в отдельные переменные, где n - количество чисел, 
     s - искомое число и chisla - список введенных чисел"""
@@ -44,10 +43,12 @@ def main():
 
     """Вызов рекурентной функции для составления всевозможных комбинаций знаков и проверка на присутстыие решения"""
     if recurs(chisla[1:], chisla[0], '', s):
-        print(
-            krasiviy_vyvod(chisla, g_otv, s, n))  # Если решение есть, то вызывается функция делающаяя необходимый вывод
+        f.write('\n' + krasiviy_vyvod(chisla, g_otv, s,
+                                      n))  # Если решение есть, то вызывается функция делающаяя необходимый вывод
+        f.close()
     else:
-        print('ns')  # Иначе - no solution
+        f.write('\n ns')  # Иначе - no solution
+        f.close()
 
 
 g_otv = ''
