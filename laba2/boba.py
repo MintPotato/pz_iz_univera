b7e4c5de6c126a7db98beb867a2776cdcca51c62
ginger dog <104458002+MintPotato@users.noreply.github.com>
2023-01-17 07:31:54 +0300

    Update boba.py

--- laba2/boba.py
+++ laba2/boba.py
@@ -6,8 +6,8 @@ def main():
     global L, N
     f = open('input.txt')
     a = list(f)
-    NLK, figure_place = list(map(int, a[0].split())), a[1:]
     f.close()
+    NLK, figure_place = list(map(int, a[0].split())), a[1:]
     start_time = time.time()
     L, N = NLK[1], NLK[0]
 
@@ -16,7 +16,7 @@ def main():
     for i in range(NLK[-1]):
         coords = list(map(int, figure_place[i].split()))
         ishod_doska[coords[0]][coords[1]] = '#'
-        ishod_doska = boi_figuri(ishod_doska, coords[0], coords[1], flag=True)
+        ishod_doska = boi_figuri(ishod_doska, coords[0], coords[1], if_ishod_doska=True)
 
     svobodn_mesta = 0
 
@@ -24,10 +24,10 @@ def main():
         svobodn_mesta += ishod_doska[i].count(0)
         ishod_doska[i] = tuple(ishod_doska[i])
 
-    if svobodn_mesta > L:
+    if svobodn_mesta >= L:
         f1 = open('output.txt', 'w')
 
-        rasstanovka_figur(tuple(ishod_doska), 0, f1, 0, flag=False)
+        rasstanovka_figur(tuple(ishod_doska), 0, f1, 0, if_recurs=False)
         f1.close()
         if kolvo_otvet:
             print(kolvo_otvet)
@@ -40,28 +40,23 @@ def main():
 
 
 # рекурентная функция расставляющая фигуры на доску
-def rasstanovka_figur(doska: tuple[tuple], cnt: int, file, a=0, b=0, flag=True):
+def rasstanovka_figur(doska: tuple[tuple], cnt: int, file, i1: int = 0, j1: int = 0, if_recurs: bool = True):
     global L, N, otvet, console_doska, kolvo_otvet
 
-    if flag:
+    if if_recurs:
         doska = list(doska)
-        c = list(doska[a])
-        c[b] = '#'
-        doska[a] = tuple(c)
-        doska = boi_figuri(doska, a, b)
+        matrix_string = list(doska[i1])
+        matrix_string[j1] = '#'
+        doska[i1] = tuple(matrix_string)
+        doska = boi_figuri(doska, i1, j1)
+
     # если количество расставленных фигур, не включая исходные, меньше заданного, то продолжаем расставлять
     if cnt < L:
-        for i in range(a, N):
-            for j in range(b, N):
+        for i in range(i1, N):
+            for j in range(j1, N):
                 if doska[i][j] == 0:
-                    # dop_doska = copy.copy(doska)
-                    # c = list(dop_doska[i])
-                    # c[j] = '#'
-                    # dop_doska[i] = tuple(c)
-                    # dop_doska = boi_figuri(dop_doska, i, j)
-
                     rasstanovka_figur(doska, cnt + 1, file, i, j)
-            b = 0
+            j1 = 0
 
     # если количество фигур выставленных на доске равно искомому, то записываем координаты фигур в ответ
     elif cnt == L:
@@ -74,7 +69,7 @@ def rasstanovka_figur(doska: tuple[tuple], cnt: int, file, a=0, b=0, flag=True):
 
 
 # функция возвращающая координаты фигур расставленных на доске
-def figuri_na_doske(doska: list[list]) -> list[tuple]:
+def figuri_na_doske(doska: tuple[tuple]) -> str:
     coords = ''
     for i in range(N):
         for j in range(N):
@@ -85,8 +80,8 @@ def figuri_na_doske(doska: list[list]) -> list[tuple]:
 
 
 # zapoln kletok boya
-def boi_figuri(doska: list[tuple], i: int, j: int, flag=False) -> list[list]:
-    if not flag:
+def boi_figuri(doska: tuple[tuple], i: int, j: int, if_ishod_doska: bool = False) -> tuple[tuple]:
+    if not if_ishod_doska:
         doska = [list(i) for i in doska]
     for a in range(1, 4):
 
@@ -113,12 +108,12 @@ def boi_figuri(doska: list[tuple], i: int, j: int, flag=False) -> list[list]:
         if 0 <= j + a < N:
             doska[i][j + a] = '*'
 
-    if not flag:
+    if not if_ishod_doska:
         doska = [tuple(i) for i in doska]
     return doska
 
 
-def vyvod_doski_v_konsol(doska):
+def vyvod_doski_v_konsol(doska: tuple[tuple]):
     doska = [list(i) for i in doska]
     for stroka in doska:
         stroka = ' '.join([str(i) for i in stroka])
@@ -129,6 +124,5 @@ L = 0
 N = 0
 console_doska = False
 kolvo_otvet = 0
-otvet = set()
 if __name__ == '__main__':
     main()
